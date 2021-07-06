ab60214c7c34ea339736273edee76bea29378926
caue <cziinff01@gmail.com>
2021-07-06 23:11:21 +0000

    adiciona tabuada ao javascript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert('ola mundo')
+var tabuada = prompt("entre com um numero para a tabuada")
+tabuada = Number(tabuada)
+var resposta = ""
+
+for(i=0;i<11;i=i+1){
+    resposta = resposta + tabuada +"x" + i + "=" + (tabuada*i) + " "
+}
+
+alert(resposta)
